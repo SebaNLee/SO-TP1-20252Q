6a60492ce5e3ef8a0941f059b3fd62315264ed6f
Sebalee_ZephyrusM16 <slee@itba.edu.ar>
2025-08-20 19:32:28 -0300

    chore: add player.c in view.c

--- src/view.c
+++ src/view.c
@@ -30,6 +30,12 @@ int main() {
     printf("Juego terminado: %d\n", state->isGameOver);
     printf("Dimensiones tablero: %u x %u\n", state->width, state->height);
 
+    // este me sirve de debug de player.c
+    printf("\n");
+    printf("\n");
+    printf("###  validMoves P1: %d    ###\n", state->players[0].validMoves);
+    printf("###  invalidMoves P1: %d  ###\n", state->players[0].validMoves);
+    printf("\n");
 
 
 
