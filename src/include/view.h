1c0bf3ef19ec586f956f70ecc5fadd9436317d8e
Sebalee <153123266+SebaNLee@users.noreply.github.com>
2025-08-23 19:07:25 -0300

    chore: merge conflicts

--- src/include/view.h
+++ src/include/view.h
@@ -1,6 +1,15 @@
 #ifndef VIEW_H
 #define VIEW_H
 
+#include "structs.h"
+#include "shm.h"
+
+
+void printHeader(int columns);
+
+void printTableContent(GameState * state);
+
+void printBase(int columns);
 
 
 
--- src/view.c
+++ src/view.c
@@ -1,6 +1,4 @@
-
-#include "structs.h"
-#include "shm.h"
+#include "view.h"
 
 #define RESET   "\033[0m"
 #define RED     "\033[41m"
@@ -19,47 +17,45 @@ void printHeader(int columns) {
         printf("═══╗\n");
 }
 
-void printTableContent(int height, int columns, GameState * game) {
+void printTableContent(GameState * state) {
 
     const char* colors[] = {RED, BLUE, GREEN, YELLOW, MAGENTA, CYAN};
 
-    // Prueba 
-    int testBoard[10][10];
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            testBoard[i][j] = i;
-        }
-    }
-
-    testBoard[0][0] = -1;
-    testBoard[0][1] = -1;
-    testBoard[1][2] = -1;
-    testBoard[1][1] = -1;
-    testBoard[5][5] = -2;
-    testBoard[5][6] = -2;
-    testBoard[5][7] = -2;
-    testBoard[6][7] = -2;
-    testBoard[7][7] = -2;
-    testBoard[8][8] = -2;
+    int height = state->height;
+    int width = state->width;
+    int * board = state->board;
+    
+    printHeader(width);
     
 
-    for (int i = 0; i < game->height; i++) {
-        for (int j = 0; j < game->width; j++) {
-            int val = testBoard[i][j];                          // Cambiar al tablero real, testBoard --> game->board[i * game->width + j]
-            if (val < 0)
-                printf("║%s%s %s", colors[-val - 1], ((i==1 && j==1) || (i==8 && j==8)) ? "🦆" : "  ",RESET);       // Hardcodeado para mostrar posicion actual
-            else 
-                printf("║ %d ", val); 
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            
+            int val = board[i * width + j];
+
+            // TODO, acceder al array de Players para obtener la jugada actual (e imprimir como una cabeza) 🦆
+            if(val <= 0)
+            {
+                printf("║%s   %s", colors[abs(val)], RESET); // TODO expandir vector de colores a jugadores máximos
+            }
+            else
+            {
+                printf("║ %d ", val);
+            }
+
+            
         }
         printf("║\n");
-        if (i+1 < game->height) {
+        if (i+1 < state->height) {
             printf("╠═══");
-            for (int j = 1; j < game->width; j++) {
+            for (int j = 1; j < width; j++) {
                 printf("╬═══");
             }
             printf("╣\n");
         } 
     }
+
+    printBase(width);
 }
 
 void printBase(int columns) {
@@ -85,41 +81,25 @@ int main() {
         // esperar aviso de master.c
         sem_wait(&sync->A);
 
-    // TODO BORRAR, esto es para testear acceso a memoria compartida
+        // TODO BORRAR, esto es para testear acceso a memoria compartida
         printf("Número de jugadores: %u\n", state->numPlayers);
         printf("Juego terminado: %d\n", state->isGameOver);
         printf("Dimensiones tablero: %u x %u\n", state->width, state->height);
 
         // este me sirve de debug de player.c
-        printf("\n");
-        printf("\n");
         printf("###  validMoves P1: %d    ###\n", state->players[0].validMoves);
         printf("###  invalidMoves P1: %d  ###\n", state->players[0].invalidMoves);
         printf("\n");
 
-        
-        printf("%p\n", (void *) state->board);
-        printf("\n");
-
-
-        printHeader(state->width);
-
-        printTableContent(state->height, state->width, state);
-
-        printBase(state->width);
+        printTableContent(state);
 
         sem_post(&sync->B);
     }
     
 
-
-
-    
-
     sem_post(&sync->B); // TODO este tal vez está de más
 
     
-
     // desconectar de mem compartida
     munmap(state, sizeof(GameState));
     munmap(sync, sizeof(GameSync));
