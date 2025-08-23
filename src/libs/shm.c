d5fde7c8551cc078e1850ad6434df501046e6ebe
Sebalee <153123266+SebaNLee@users.noreply.github.com>
2025-08-23 17:45:36 -0300

    fix!: shm.c GameState now with write permissions

--- src/libs/shm.c
+++ src/libs/shm.c
@@ -2,16 +2,19 @@
 
 
 
+
+// TODO tal vez se tenga que hacer con permisos de read only o con write tmb
+
 GameState * getGameState()
 {
-    int fd_state = shm_open("/game_state", O_RDONLY, 0);
+    int fd_state = shm_open("/game_state", O_RDONLY, 0); // TODO ojo que esto está en RDONLY
     if (fd_state == -1)
     {
         perror("shm_open /game_state");
         exit(1);
     }
 
-    GameState *state = mmap(NULL, sizeof(GameState), PROT_READ, MAP_SHARED, fd_state, 0);
+    GameState *state = mmap(NULL, sizeof(GameState), PROT_READ, MAP_SHARED, fd_state, 0); // TODO ojo que esto está en RDONLY
     if (state == MAP_FAILED)
     {
         perror("mmap /game_state");
@@ -26,14 +29,14 @@ GameState * getGameState()
 
 GameSync * getGameSync()
 {
-    int fd_sync = shm_open("/game_sync", O_RDONLY, 0);
+    int fd_sync = shm_open("/game_sync", O_RDWR, 0);
     if (fd_sync == -1)
     {
         perror("shm_open /game_sync");
         exit(1);
     }
 
-    GameSync *sync = mmap(NULL, sizeof(GameSync), PROT_READ, MAP_SHARED, fd_sync, 0);
+    GameSync *sync = mmap(NULL, sizeof(GameSync), PROT_READ | PROT_WRITE, MAP_SHARED, fd_sync, 0);
     if (sync == MAP_FAILED)
     {
         perror("mmap /game_sync");
