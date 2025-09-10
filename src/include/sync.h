3415f0b7131a0d511550cccf0cec3eba8d503936
Sebalee <slee@itba.edu.ar>
2025-09-10 00:54:33 -0300

    feat!: add sync lib

---
+++ src/include/sync.h
@@ -0,0 +1,13 @@
+#ifndef SYNC_H
+#define SYNC_H
+
+#include "structs.h"
+
+void masterEntrySync(GameSync * sync);
+void masterExitSync(GameSync * sync);
+void playerEntrySync(GameSync * sync);
+void playerExitSync(GameSync * sync);
+void moveProcessedPostSync(GameSync * sync, int i);
+void moveProcessedWaitSync(GameSync * sync, int i);
+
+#endif
