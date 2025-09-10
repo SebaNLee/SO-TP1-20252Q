4d5fa1538b17d98b367b8dc0060438692de7c925
Sebalee <slee@itba.edu.ar>
2025-09-09 23:27:48 -0300

    Revert "feat: some mutex name change"
    This reverts commit 77e4aa0bf9bda81266953fd2b951b280e14630c8.

--- src/include/structs.h
+++ src/include/structs.h
@@ -44,8 +44,8 @@ typedef struct {
      */
     sem_t view_writing_done; 
 
-    sem_t mutex_master_entry; // indica que el master quiere acceder exclusivamente a GameState
-    sem_t mutex_game_state; // acceso exclusivo para GameState
+    sem_t mutex_readers; //protege el acceso al contador F (para sumar/restar lectores) 
+    sem_t mutex_writer; //se bloquea cuando alguien escribe (el máster)
     sem_t mutex_counter; //se usa para evitar que lectores entren mientras el escritor espera
     unsigned int numReaders;//contador de cuántos jugadores están leyendo.
     sem_t send_move[9]; // Le indican a cada jugador que puede enviar 1 movimiento
