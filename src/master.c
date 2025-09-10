facd42e951fb7503a292b985e0817327a4c39932
Sebalee <slee@itba.edu.ar>
2025-09-10 14:46:54 -0300

    feat: add missing free

--- src/master.c
+++ src/master.c
@@ -126,6 +126,7 @@ int main(int argc, char const *argv[]) {
     // libero pipes
     freePipes(pipesfd, state->numPlayers);
 
+    freeGameState(state);
     freeGameSync(sync);
     
     closeSHM(state, sizeof(GameState) + params.width * params.height * sizeof(int));
